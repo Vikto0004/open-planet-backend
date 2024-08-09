78621cfb541c2116daedad95b2e8e9c003dfc74d
Artem Mironov <31404136+artemko77788@users.noreply.github.com>
2024-08-09 17:22:17 +0300

    fix problems

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 const app = express();
 const port = 3000;
+app.post("/", (req, res) => {
+    console.log("hello");
+    res.send("Hello, World!");
+});
 app.listen(port, () => {
     console.log("port run", port);
 });
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  console.log("hello");
+  res.json("Hello, World!");
+});
+
 app.listen(port, () => {
   console.log("port run", port);
 });
